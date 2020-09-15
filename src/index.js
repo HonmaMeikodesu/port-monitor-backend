e80a14876c8a7a2418b5048557d090b8d5981373
HonmaMeiko <17mwang2@stu.edu.cn>
2020-09-15 17:14:59 +0800

    init backend

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cp = require('child_process');
+const app = express();
+const PORT = 8003;
+app.get('/get_port_data', (req, res) => {
+  cp.exec(`iftop -s -t ${req.params?.time ? req.params.time : 5}`, (err, stdout, stderr) => {
+    if (err) {
+      res.end('request error');
+    }
+    res.end(stdout);
+  })
+})
+
+app.listen(PORT);
