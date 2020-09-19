30d8fe3e88e56c5ec984bf2f5f0f96f3ada01e30
HonmaMeiko <17mwang2@stu.edu.cn>
2020-09-19 16:33:15 +0800

    init time display

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cp = require('child_process');
 const path = require('path');
+const redis = require('redis');
+const { promisify } = require("util");
+const client = redis.createClient();
+
 const app = express();
 const PORT = 8003;
 
@@ -25,15 +29,25 @@ app.get('/get_ip_tables/', (req, res) => {
     if (err) {
       console.error(err);
       res.end('request error');
-  }
-  res.end(stdout);
+    }
+    const reg = /dpt\:(\d+)/g;
+    const allPorts = stdout.match(reg).map(port => port.replace('dpt:', ''));
+    const filteredPorts = allPorts.filter((val, idx) => allPorts.indexOf(val) === idx);
+    const promiseList = [];
+    filteredPorts.forEach(port => {
+      promiseList.push(promisify(client.get(port)).bind(client));
+    })
+    Promise.all(promiseList).then(res => {
+      console.log(res);
+    })
+    res.end(stdout);
   })
 })
 
 app.get('/set_ip_tables/:port', (req, res) => {
   const port = Number.parseInt(req.params.port);
   if (port <= 1000 || port > 65535) res.end();
-  cp.exec(`iptables -A INPUT -p tcp --dport ${port} && iptables -A OUTPUT -p tcp --sport ${port}`, (err, stdout, stderr) => {
+  cp.exec(`iptables -A INPUT -p tcp --dport ${port} && iptables -A OUTPUT -p tcp --sport ${port} && cat ${port}`, (err, stdout, stderr) => {
     if (err) {
       res.end('request error')
     } else {
@@ -46,16 +60,6 @@ app.get('/rm_ip_tables/:port', (req, res) => {
   const port = Number.parseInt(req.params.port);
   if (port <= 1000 || port > 65535) res.end();
   cp.exec(`iptables -D INPUT -p tcp --dport ${port} && iptables -D OUTPUT -p tcp --sport ${port}`, (err, stdout, stderr) => {
-    if (err) {
-      res.end('request error')
-    } else {
-      res.end('200')
-    }
-  })
-})
-
-app.get('/reset_ip_tables/', (req, res) => {
-  cp.exec(`iptables -Z INPUT && iptables -Z OUTPUT`, (err, stdout, stderr) => {
     if (err) {
       res.end('request error')
     } else {
